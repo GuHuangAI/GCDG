09e990aa240acda3fdca12212efb19c74b4d2404
GuHuangAI <55237825+GuHuangAI@users.noreply.github.com>
2022-03-18 14:11:09 +0800

    Update run_test2.sh

--- run_test2.sh
+++ run_test2.sh
@@ -1,8 +1,8 @@
 #NETWORK='Dancerevolution_2Transformer'
 #NETWORK='2tcnlstm'
 NETWORK='CrossModalTr'
-VERSION='5'
-EPOCH='1500'
-MODEL_DIR='/export/home/lg/huang/code/music2dance_condition/checkpoints/'${NETWORK}'_V'${VERSION}
-CLASSIFIER_PATH="/export/home/lg/huang/code/music2dance_condition/classifier/best_model_512_512dim_60frame_rnn"
+VERSION='13'
+EPOCH='1600'
+MODEL_DIR='./checkpoints/'${NETWORK}'_V'${VERSION}'_3'
+CLASSIFIER_PATH="./classifier/best_model_512_512dim_100frame_rnn_smpl"
 CUDA_VISIBLE_DEVICES=1 python core/test2.py ${MODEL_DIR} ${CLASSIFIER_PATH} ${NETWORK} ${EPOCH} ${VERSION} --regressive_len 60
