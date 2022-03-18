01e062dfcd461a293ef5c21af25649f8a82a4675
GuHuangAI <55237825+GuHuangAI@users.noreply.github.com>
2022-03-18 14:08:02 +0800

    Update run_cmtr.sh

--- run_cmtr.sh
+++ run_cmtr.sh
@@ -1,6 +1,6 @@
 NETWORK='CrossModalTr'
 #NETWORK='2tcnlstm'
-SAVE_VERSION='6_2'
+SAVE_VERSION='13'
 SAVE_DIR='/export/home/lg/huang/code/music2dance_condition/checkpoints/'${NETWORK}'_V'${SAVE_VERSION}
 LOSS_VERSION='2'
-CUDA_VISIBLE_DEVICES=0,1,2,3 python core/train.py ${SAVE_DIR} ${NETWORK} ${SAVE_VERSION} ${LOSS_VERSION}
+CUDA_VISIBLE_DEVICES=0,1,2,3 python core/train.py ${SAVE_DIR} ${NETWORK} ${SAVE_VERSION} ${LOSS_VERSION} --classifier_path "./classifier/best_model_512_512dim_100frame_rnn_smpl" 
